12ad4158b47346e54beb38c15d421a907f871936
tn <powertn@126.com>
2019-10-04 00:06:01 +0800

    完成secutity简单登入

---
+++ secutity01/src/main/java/com/github/powerttt/Secutity01Appliction.java
@@ -0,0 +1,20 @@
+package com.github.powerttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author tongning
+ * @Date 2019/10/3 0003
+ * function:<
+ * <p>
+ * >
+ */
+@SpringBootApplication
+public class Secutity01Appliction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Secutity01Appliction.class);
+    }
+
+}
