7b432325359ceb0e36ce789a6e5fd93d27eed33a
tn <powertn@126.com>
2019-10-08 14:25:49 +0800

    添加登入登出功能

--- secutity02/src/main/java/com/github/powerttt/security02/config/WebSecurityConfig.java
+++ secutity02/src/main/java/com/github/powerttt/security02/config/WebSecurityConfig.java
@@ -1,10 +1,7 @@
 package com.github.powerttt.security02.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -20,8 +17,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        // 关闭csrf
+       http.csrf().disable();
+
         // 配置登录页面
-        http.formLogin();
+        http.formLogin().loginPage("/login").permitAll();
+
+        // 配置登录成功后的默认页面
+        http.formLogin().defaultSuccessUrl("/home");
+
+        // 配置登录页面
+        http.logout().permitAll();
 
         //授权配置
         http.authorizeRequests().anyRequest().fullyAuthenticated();
--- secutity02/src/main/java/com/github/powerttt/security02/controller/HomeController.java
+++ secutity02/src/main/java/com/github/powerttt/security02/controller/HomeController.java
@@ -2,19 +2,19 @@ package com.github.powerttt.security02.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @Author tongning
- * @Date 2019/10/3 0003
+ * @Date 2019/10/8 0008
  * function:<
  * <p>
  * >
  */
+
 @Controller
-public class LoginController {
-
-    @GetMapping("/login")
-    public String login(){
-        return "login";
+public class HomeController {
+    @GetMapping("/home")
+    public String home(){
+        return "home";
     }
-
 }
